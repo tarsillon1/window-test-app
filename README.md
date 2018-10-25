0486cb1006aef915fd1b42c3bc1165f2bf8151ed
tarsillon1 <ricklypop@gmail.com>
2018-10-24 23:25:10 -0400

    Clean up.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 This app is for testing window open close resource consumption on different platforms.
 
 Currently supports Windows only and testing on Chrome, OpenFin and Electron.
+
+## Getting Started
+1. Install project dependencies:  `npm run install`
+2. Build the app: `npm run build`
+3. Configure `.env`.
+4. Run the app: `npm run start`
+5. Go to `localhost:8443` to run tests.
--- routes/event.js
+++ routes/event.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const axios = require("axios");
 const childProcess = require("child_process");
-const util = require("util");
+const { promisify } = require("util");
 
 const express = require("express");
 const router = express.Router();
 
-const EXEC = util.promisify(childProcess.exec);
+const EXEC = promisify(childProcess.exec);
 const LOG_STREAM_ID = [...Array(10)]
   .map(i => (~~(Math.random() * 36)).toString(36))
   .join("");
@@ -21,16 +21,30 @@ function alias(processName) {
   ) {
     return processName;
   }
+
   if (processName.toLowerCase().includes("openfin")) {
     name += "OpenFin Process ";
-  }
-  if (processName.toLowerCase().includes("chrome")) {
+  } else if (processName.toLowerCase().includes("chrome")) {
+    name += "Chrome Process ";
   } else {
     name += "Electron Process ";
   }
-  if (processName.toLowerCase().includes("-startup-url")) {
+
+  if (processName.toLowerCase().includes("--type=watcher")) {
+    name += "Watcher ";
+  }
+
+  if (processName.toLowerCase().includes("--type=crashpad-handler")) {
+    name += "Crash Handler ";
+  }
+
+  if (
+    processName.toLowerCase().includes("-startup-url") ||
+    !processName.toLowerCase().includes("--")
+  ) {
     name += "Browser ";
   }
+
   if (processName.toLowerCase().includes("--type=gpu-process")) {
     name += "GPU ";
   } else if (processName.toLowerCase().includes("--type=renderer")) {
@@ -41,6 +55,7 @@ function alias(processName) {
     if (stripToken)
       name += stripToken.substring(0, stripToken.indexOf(" ") + 1);
   }
+
   if (processName.toLowerCase().includes("rvm")) {
     name += "RVM";
   }
@@ -48,7 +63,7 @@ function alias(processName) {
 }
 
 // Use Get-Process to get windows process memory. Then parse the output to fit metric log interface.
-async function getLog(processName, events) {
+async function getWindowsLog(processName) {
   let { stdout } = await EXEC(
     `powershell.exe -ExecutionPolicy ByPass -file ${path.resolve(
       __dirname,
@@ -61,69 +76,89 @@ async function getLog(processName, events) {
     stdout = index !== -1 ? stdout.substring(index) : null;
     return stdout;
   };
+
+  let createdAt = new Date().toISOString();
+  let totalWorkingSet = 0;
   let totalPrivateWorkingSet = 0;
   let relatedLogs = [];
   while (getNext()) {
     let row = stdout.substring(
       0,
-      stdout.indexOf("\r") !== -1 ? stdout.indexOf("\r") : stdout.length
+      stdout.substring(1).indexOf("@") !== -1
+        ? stdout.substring(1).indexOf("@")
+        : stdout.length
     );
     let name = stdout
-      .substring("@{CommandLine=".length, row.lastIndexOf(" "))
+      .substring("@{CommandLine=".length, row.lastIndexOf("}") + 1)
       .trim();
-    stdout = stdout.substring(row.lastIndexOf(" ")).trim();
+    stdout = stdout.substring(row.lastIndexOf("}") + 1).trim();
 
     // The private working set ends before the start of the next line.
     let privateWorkingSet = Number.parseInt(
-      stdout.substring(
-        0,
-        stdout.indexOf("\r") !== -1
-          ? stdout.indexOf("\r")
-          : stdout.substring(stdout.lastIndexOf(" "), stdout.length)
-      )
+      stdout.substring(0, stdout.indexOf(" "))
     );
     totalPrivateWorkingSet += privateWorkingSet ? privateWorkingSet : 0;
 
+    let workingSet = Number.parseInt(
+      stdout.substring(stdout.indexOf(" "), row.length).trim()
+    );
+    totalWorkingSet += workingSet ? workingSet : 0;
+
     relatedLogs.push({
+      createdAt,
       fields: {
         logStreamId: LOG_STREAM_ID,
         processName: alias(name),
         platform: "Windows",
         privateWorkingSet,
-        events
+        workingSet
       }
     });
   }
 
   return {
+    createdAt,
     fields: {
       logStreamId: LOG_STREAM_ID,
-      processName: platform,
+      processName,
       platform: "Windows",
       privateWorkingSet: totalPrivateWorkingSet,
-      events
+      workingSet: totalWorkingSet
     },
     relatedLogs
   };
 }
 
-let platform,
+async function getLog(processName) {
+  switch (process.platform) {
+    case "win32":
+      return await getWindowsLog(processName);
+  }
+}
+
+let track,
   events = [];
 
 // Log periodically.
 (async () => {
   let logs = [];
   while (true) {
-    if (!platform) return;
+    await promisify(setTimeout)(1000);
+    if (!track) continue;
 
-    let log = await getLog(platform, events);
+    let log = await getLog(track, events);
     logs.push(log);
 
+    // Attach recent events.
+    log.fields.events = events;
+    log.relatedLogs.forEach(log => (log.fields.events = events));
+    events = [];
+
     // Ingest using Aggregator.
-    if (logs.length >= 30) {
-      axios.post(
-        `${process.env.AGGREGATOR_URL}/aggregator/collect?apiToken=${
-          process.env.AGGREGATOR_API_TOKEN
+    if (logs.length >= 5) {
+      await axios.post(
+        `${process.env.INGESTER_URL}/ingester/collect?apiToken=${
+          process.env.INGESTER_API_TOKEN
         }&schema=com.aggregator.log.trigger`,
         {
           logs
@@ -134,15 +169,19 @@ let platform,
   }
 })();
 
-// Set logger platform.
-router.post("/platform", function(req, res, next) {
-  platform = req.body.platform;
+// Set logger process to track.
+router.post("/track", function(req, res, next) {
+  track = req.body.track;
+
+  res.status(200).end();
 });
 
 // Trigger events to be logged.
 router.post("/event", function(req, res, next) {
   let event = req.body.event;
   events.push(event);
+
+  res.status(200).end();
 });
 
 module.exports = router;
